e78132432bf0dea49541d57263e4f8e5854ab32d
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-07-30 12:21:25 -0300

    update xdg-user config

--- conf.d/xdg-user.fish
+++ conf.d/xdg-user.fish
@@ -1,9 +1,3 @@
-set -x XDG_CACHE_HOME $HOME/.cache
-set -x XDG_CONFIG_HOME $HOME/.config
-set -x XDG_DATA_HOME $HOME/.local/share
-set -x XDG_DESKTOP_DIR $HOME/Desktop
-set -x XDG_DOWNLOAD_DIR $HOME/Downloads
-set -x XDG_DOCUMENTS_DIR $HOME/Documents
-set -x XDG_MUSIC_DIR $HOME/Music
-set -x XDG_PICTURES_DIR $HOME/Pictures
-set -x XDG_VIDEOS_DIR $HOME/Videos
+set -q XDG_CACHE_HOME; and set -x XDG_CACHE_HOME $HOME/.cache
+set -q XDG_CONFIG_HOME; and set -x XDG_CONFIG_HOME $HOME/.config
+set -q XDG_DATA_HOME; and set -x XDG_DATA_HOME $HOME/.local/share
