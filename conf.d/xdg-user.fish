bdf20ef5bc39f94e3389e8794174dc885c6e3ce3
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-08-04 10:40:05 -0300

    make xdg beauty

--- conf.d/xdg-user.fish
+++ conf.d/xdg-user.fish
@@ -1,3 +1,3 @@
+set -q XDG_DATA_HOME; and set -x XDG_DATA_HOME $HOME/.local/share
 set -q XDG_CACHE_HOME; and set -x XDG_CACHE_HOME $HOME/.cache
 set -q XDG_CONFIG_HOME; and set -x XDG_CONFIG_HOME $HOME/.config
-set -q XDG_DATA_HOME; and set -x XDG_DATA_HOME $HOME/.local/share
